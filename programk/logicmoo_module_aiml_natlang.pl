2f2f430133cbf263c45211d5d6c5fdeb1ae048ce
DouglasRMiles <dmiles@daxtron.com>
2010-12-21 04:11:07 -0800

    ProgramK - correct tokenizeInput/2

--- programk/logicmoo_module_aiml_natlang.pl
+++ programk/logicmoo_module_aiml_natlang.pl
@@ -13,15 +13,31 @@
 % ===============================================================================================
 % Split input into many words
 % ===============================================================================================
+toString_atom(Input,Atom):-toCycApiExpression(Input,Out,[]),ctrace,string_to_atom(Atom,Out).
 
-tokenizeInput(String,Tokens):-notrace(tokenizeInput0(String,Tokens)).
-tokenizeInput0(Input,Input):-var(Input),!.
+tokenizeInput(Input,Tokens):- notrace(is_string(Input)),toString_atom(Input,Atom),!,tokenizeInput(Atom,Tokens).
+%%tokenizeInput(String,Tokens):-hotrace(tokenizeInput0(String,Tokens)),!.
+tokenizeInput(String,Tokens):-hotrace(tokenizeInput0(String,Tokens)),Tokens\==[],ground(Tokens).
+tokenizeInput(String,Tokens):-trace,tokenizeInput0(String,Tokens),!.
+
+tokenizeInput0(Input,Tokens):-var(Input),!,Input=Tokens.
 tokenizeInput0([],[]):-!.
-tokenizeInput0(Input,Tokens):- atom(Input),error_catch(((atom_to_term(Input,Tokens,Vars),ground(Vars))),_,fail),!.
-tokenizeInput0(Input,Tokens):- atom(Input),load_structure_from_string(Input,Tokens0),(Tokens0\==[Input]->tokenizeInput0(Tokens0,Tokens);Tokens0=Tokens),!.
-tokenizeInput0(Input,Tokens):- atom(Input),atomWSplit(Input,Tokens0), (Tokens0\==[Input]->tokenizeInput0(Tokens0,Tokens);Tokens0=Tokens),!.
-tokenizeInput0([A|B],Out):- tokenizeInput0(A,AA),tokenizeInput0(B,BB),!,append(AA,BB,Out).
+tokenizeInput0(Input,Tokens):- string(Input),string_to_atom(Input,Atom),!,tokenizeInput1(Atom,Tokens).
+tokenizeInput0([A|B],Out):- tokenizeInput0(A,AA),tokenizeInput_l(B,BB),!,flatten([AA,BB],Out).
+tokenizeInput0(Input,Tokens):- atom(Input),tokenizeInput1(Input,Tokens),!.
+tokenizeInput0(Input,Tokens):- notrace(is_string(Input)),toString_atom(Input,Atom),!,tokenizeInput0(Atom,Tokens).
+tokenizeInput0(Compound,Out):-Compound=..[A|B],tokenizeInput_l(B,BB),!,Out=..[A|BB].
 tokenizeInput0(A,A).
+
+
+tokenizeInput_l(A,A):-atomic(A),!.
+tokenizeInput_l([A|B],[AA|BB]):- tokenizeInput0(A,AA),tokenizeInput_l(B,BB),!.
+
+tokenizeInput1(Input,Tokens):-  error_catch(((atom_to_term(Input,Tokens,Vars),ground(Vars))),_,fail),!.
+tokenizeInput1(Input,Tokens):-  (atom_contains(Input,'<');atom_contains(Input,'&')),
+     error_catch((string_to_structure(Input,Tokens0),(Tokens0\==[Input]->tokenizeInput0(Tokens0,Tokens);Tokens0=Tokens)),_,fail),!.
+tokenizeInput1(Input,Tokens):- atomWSplit(Input,Tokens0), (Tokens0\==[Input]->tokenizeInput0(Tokens0,Tokens);Tokens0=Tokens),!.
+tokenizeInput1(Input,Input).
 
 
 % ===============================================================================================
@@ -34,16 +50,19 @@ atomify([A|List],Result):-joinAtoms([A|List],' ',Result).
 
 joinAtoms([],_,'').
 joinAtoms(List,Sep,Result):-atomify(Sep,SepA),Sep\==SepA,!,joinAtoms(List,SepA,Result).
-joinAtoms(List,Sep,Result):-hotrace(joinAtoms0(List,Sep,Result)),!.
+joinAtoms(List,Sep,Result):-hotrace(prolog_must(joinAtoms0(List,Sep,Result))),!.
 
 joinAtoms0([],_,'').
 joinAtoms0([A],_,AA):-atomify(A,AA).
 joinAtoms0([A|More],Sep,Result):-atomify(A,AA),A\==AA,!,joinAtoms0([AA|More],Sep,Result).
 joinAtoms0([A|List],Sep,Result):-prolog_must(atomic(A)),joinAtoms1([A|List],Sep,Result).
 
-joinAtoms1([A,'\b',B|List],Sep,Result):-atomify(B,BB),atom_concat(A,BB,C),!,joinAtoms0([C|List],Sep,Result).
-joinAtoms1([A,B|List],'',Result):-atom_concat(A,B,C),!,joinAtoms1([C|List],'',Result).
-joinAtoms1([A,B|List],Sep,Result):-atom_concat(A,Sep,C),!,joinAtoms0([C,B|List],Sep,Result).
+joinAtoms1([],_,'').
+joinAtoms1([A],_,AA):-atomify(A,AA).
+%%%%joinAtoms1([A,'\b',B|List],Sep,Result):-!,joinAtoms0([B|List],Sep,ResultR),atom_concat(A,ResultR,Result).
+joinAtoms1([A,'\b',B|List],Sep,Result):-!,atomify(B,BB),atom_concat(A,BB,C),!,joinAtoms0([C|List],Sep,Result).
+joinAtoms1([A,B|List],'',Result):-!,atom_concat(A,B,C),!,joinAtoms1([C|List],'',Result).
+joinAtoms1([A,B|List],Sep,Result):-atom_concat(A,Sep,C),joinAtoms1([B|List],Sep,ResultR),atom_concat(C,ResultR,Result).
 joinAtoms1(List,Sep,Result):- debugOnError(atomic_list_concat(List,Sep,Result)),!.
 
 % ===============================================================================================

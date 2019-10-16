70fca4ef1963cef65cabeac0f40e248dd2d63792
logicmoo <logicmoo@gmail.com>
2019-10-16 11:02:48 -0700

    imto the pack layout

---
+++ pack.pl
@@ -0,0 +1,11 @@
+name(programk).
+version('1.2.111').
+title('AIML 2.0 - Because an AIML Interpreter/Chatbot needed to exist in Prolog to!').
+keywords(['chatbot', 'aiml']).
+author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
+maintainer( 'Douglas R, Miles', 'logicmoo@gmail.com' ).
+home( 'https://github.com/TeamSPoon/programk.git' ).
+download( 'https://github.com/TeamSPoon/programk/archive/V*.zip' ).
+requires(logicmoo_utils).
+autoload(false).
+
